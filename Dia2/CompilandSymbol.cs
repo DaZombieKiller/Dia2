66e2f1b2acb8057402e444194fe193c1fef07c10
Zombie <bennyboy.private@gmail.com>
2020-12-17 00:05:10 +1000

    Work on CompilandSymbol.

--- Dia2/CompilandSymbol.cs
+++ Dia2/CompilandSymbol.cs
@@ -4,6 +4,21 @@ namespace Dia2
 {
     public class CompilandSymbol : Symbol
     {
+        /// <summary>The name of the compiler used to generate the Compiland.</summary>
+        public string CompilerName => symbol.compilerName;
+
+        /// <summary>Specifies if the Compiland contains debugging information.</summary>
+        public bool HasDebugInfo => symbol.hasDebugInfo;
+
+        /// <summary>Specifies whether the Compiland has been linked with the linker switch /LTCG (Link-time Code Generation), which aids in whole program optimization. This switch applies only to managed code.</summary>
+        public bool LinkTimeCodeGeneration => symbol.isLTCG;
+
+        /// <summary>The platform type for which the compiland was compiled.</summary>
+        public CpuType Platform => symbol.platform;
+
+        /// <summary>The file name of the compiland source file.</summary>
+        public string SourceFileName => symbol.sourceFileName;
+
         internal CompilandSymbol(Pdb pdb, IDiaSymbol symbol)
             : base(pdb, symbol)
         {
